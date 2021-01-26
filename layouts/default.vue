ce180d34890025739c6a9e0e4fd8eca69898de47
shouya <idb8bzputt@gmail.com>
2021-01-26 17:08:23 +0900

    nuxtプロジェクトを作成

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app class="bg">
+    <nuxt />
+  </v-app>
+</template>
+
+<style scoped>
+.bg {
+  background-image: url('~@/assets/img/bg.jpg');
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Head />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Head from '~/components/layout/Header.vue'
+
+export default defineComponent({
+  components: {
+    Head,
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
