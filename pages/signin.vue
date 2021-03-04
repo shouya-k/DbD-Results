0e58ba670c2d8a0ae00e5415a22a3cda575e7e3e
shouya <idb8bzputt@gmail.com>
2021-03-04 15:37:55 +0900

    signinページを作成 close #7

---
+++ pages/signin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Head />
+    <Form />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Head from '~/components/layout/LoginHeader.vue'
+import Form from '~/components/contents/SigninForm.vue'
+
+export default defineComponent({
+  components: {
+    Head,
+    Form,
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
