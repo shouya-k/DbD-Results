8693bb085c1d588d677e20492f3e4fe8057b44e0
光菅翔也 <idb8bzputt@gmail.com>
2021-04-06 17:36:30 +0900

    localStorageの処理を一旦削除

--- compositions/common/useFormModal.ts
+++ compositions/common/useFormModal.ts
@@ -34,51 +34,6 @@ export const useFormModal = () => {
     modal.killerId = event.id
   }
 
-  const getKillerParkDataFromLocalStorege = () => {
-    if (localStorage.parkId01) {
-      modal.parkId01 = localStorage.killerParkId01
-      modal.parkName01 = localStorage.killerParkName01
-      modal.parkImage01 = localStorage.killerParkImage01
-    }
-    if (localStorage.parkId02) {
-      modal.parkId02 = localStorage.killerParkId02
-      modal.parkName02 = localStorage.killerParkName02
-      modal.parkImage02 = localStorage.killerParkImage02
-    }
-    if (localStorage.parkId03) {
-      modal.parkId03 = localStorage.killerParkId03
-      modal.parkName03 = localStorage.killerParkName03
-      modal.parkImage03 = localStorage.killerParkImage03
-    }
-    if (localStorage.parkId04) {
-      modal.parkId04 = localStorage.killerParkId04
-      modal.parkName04 = localStorage.killerParkName04
-      modal.parkImage04 = localStorage.killerParkImage04
-    }
-  }
-  const getSurvivorParkDataFromLocalStorege = () => {
-    if (localStorage.parkId01) {
-      modal.parkId01 = localStorage.survivorParkId01
-      modal.parkName01 = localStorage.survivorParkName01
-      modal.parkImage01 = localStorage.survivorParkImage01
-    }
-    if (localStorage.parkId02) {
-      modal.parkId02 = localStorage.survivorParkId02
-      modal.parkName02 = localStorage.survivorParkName02
-      modal.parkImage02 = localStorage.survivorParkImage02
-    }
-    if (localStorage.parkId03) {
-      modal.parkId03 = localStorage.survivorParkId03
-      modal.parkName03 = localStorage.survivorParkName03
-      modal.parkImage03 = localStorage.survivorParkImage03
-    }
-    if (localStorage.parkId04) {
-      modal.parkId04 = localStorage.survivorParkId04
-      modal.parkName04 = localStorage.survivorParkName04
-      modal.parkImage04 = localStorage.survivorParkImage04
-    }
-  }
-
   const showParkModal01 = () => (modal.parkModal01 = true)
   const showParkModal02 = () => (modal.parkModal02 = true)
   const showParkModal03 = () => (modal.parkModal03 = true)
@@ -89,109 +44,29 @@ export const useFormModal = () => {
   const hiddenParkModal03 = () => (modal.parkModal03 = false)
   const hiddenParkModal04 = () => (modal.parkModal04 = false)
 
-  const selectKillerPark01 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
+  const selectPark01 = (event: any): void => {
     modal.parkModal01 = false
     modal.parkId01 = event.id
     modal.parkName01 = event.name
     modal.parkImage01 = event.url
-    localStorage.killerParkId01 = event.id
-    localStorage.killerParkName01 = event.name
-    localStorage.killerParkImage01 = event.url
   }
-  const selectKillerPark02 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
+  const selectPark02 = (event: any): void => {
     modal.parkModal02 = false
     modal.parkId02 = event.id
     modal.parkName02 = event.name
     modal.parkImage02 = event.url
-    localStorage.killerParkId02 = event.id
-    localStorage.killerParkName02 = event.name
-    localStorage.killerParkImage02 = event.url
   }
-  const selectKillerPark03 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
+  const selectPark03 = (event: any): void => {
     modal.parkModal03 = false
     modal.parkId03 = event.id
     modal.parkName03 = event.name
     modal.parkImage03 = event.url
-    localStorage.killerParkId03 = event.id
-    localStorage.killerParkName03 = event.name
-    localStorage.killerParkImage03 = event.url
   }
-  const selectKillerPark04 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
+  const selectPark04 = (event: any): void => {
     modal.parkModal04 = false
     modal.parkId04 = event.id
     modal.parkName04 = event.name
     modal.parkImage04 = event.url
-    localStorage.killerParkId04 = event.id
-    localStorage.killerParkName04 = event.name
-    localStorage.killerParkImage04 = event.url
-  }
-  const selectSurvivorPark01 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
-    modal.parkModal01 = false
-    modal.parkId01 = event.id
-    modal.parkName01 = event.name
-    modal.parkImage01 = event.url
-    localStorage.survivorParkId01 = event.id
-    localStorage.survivorParkName01 = event.name
-    localStorage.survivorParkImage01 = event.url
-  }
-  const selectSurvivorPark02 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
-    modal.parkModal02 = false
-    modal.parkId02 = event.id
-    modal.parkName02 = event.name
-    modal.parkImage02 = event.url
-    localStorage.survivorParkId02 = event.id
-    localStorage.survivorParkName02 = event.name
-    localStorage.survivorParkImage02 = event.url
-  }
-  const selectSurvivorPark03 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
-    modal.parkModal03 = false
-    modal.parkId03 = event.id
-    modal.parkName03 = event.name
-    modal.parkImage03 = event.url
-    localStorage.survivorParkId03 = event.id
-    localStorage.survivorParkName03 = event.name
-    localStorage.survivorParkImage03 = event.url
-  }
-  const selectSurvivorPark04 = (event: {
-    id: string
-    name: string
-    url: string
-  }): void => {
-    modal.parkModal04 = false
-    modal.parkId04 = event.id
-    modal.parkName04 = event.name
-    modal.parkImage04 = event.url
-    localStorage.survivorParkId04 = event.id
-    localStorage.survivorParkName04 = event.name
-    localStorage.survivorParkImage04 = event.url
   }
 
   return {
@@ -199,8 +74,6 @@ export const useFormModal = () => {
     showKillerModal,
     hiddenKillerModal,
     selectKiller,
-    getKillerParkDataFromLocalStorege,
-    getSurvivorParkDataFromLocalStorege,
     showParkModal01,
     showParkModal02,
     showParkModal03,
@@ -209,13 +82,9 @@ export const useFormModal = () => {
     hiddenParkModal02,
     hiddenParkModal03,
     hiddenParkModal04,
-    selectKillerPark01,
-    selectKillerPark02,
-    selectKillerPark03,
-    selectKillerPark04,
-    selectSurvivorPark01,
-    selectSurvivorPark02,
-    selectSurvivorPark03,
-    selectSurvivorPark04,
+    selectPark01,
+    selectPark02,
+    selectPark03,
+    selectPark04,
   }
 }
